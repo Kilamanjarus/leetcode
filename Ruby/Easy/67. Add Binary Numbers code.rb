524eaf1e20749164c5217f894a1a14d86b481895
max <max.lillie95@gmail.com>
2023-04-04 19:48:40 -0500

    approached it wrong, found a way to set the number to be either the index if it exists or 0, then went from there

--- Ruby/Easy/67. Add Binary Numbers code.rb
+++ Ruby/Easy/67. Add Binary Numbers code.rb
@@ -1,44 +1,16 @@
-# @param {String} a
-# @param {String} b
-# @return {String}
 def add_binary(a, b)
   a_index = a.length - 1
   b_index = b.length - 1
   carry = 0
-  output = []
+  sum = ""
   while a_index >= 0 || b_index >= 0 || carry > 0
-    p a[a_index]
-    p b[b_index]
-    if a[a_index].to_i + b[b_index].to_i == 2 && carry != 1
-      p "pass 2 w/o carry"
-      carry = 1
-      output << 0
-    elsif a[a_index].to_i + b[b_index].to_i == 2 && carry == 1
-      p "pass 2 w/ carry"
-      carry = 1
-      output << 1
-    elsif a[a_index].to_i + b[b_index].to_i == 1 && carry != 1
-      p "pass 1 w/o carry"
-      carry = 0
-      output << 1
-    elsif a[a_index].to_i + b[b_index].to_i == 1 && carry == 1
-      p "pass 1 w/ carry"
-      carry = 1
-      output << 0
-    elsif a[a_index].to_i + b[b_index].to_i == 0 && carry != 1
-      p "pass 0 w/o carry"
-      carry = 0
-      output << 0
-    elsif a[a_index].to_i + b[b_index].to_i == 0 && carry == 1
-      p "pass 0 w/ carry"
-      carry = 0
-      output << 1
-    end
-    p output
-    p a_index
-    p b_index
+    number_a = a_index >= 0 ? a[a_index].to_i : 0
+    number_b = b_index >= 0 ? b[b_index].to_i : 0
+    digit_sum = number_a + number_b + carry
+    sum = (digit_sum % 2).to_s + sum
+    carry = digit_sum / 2
     a_index -= 1
     b_index -= 1
   end
-  return output
+  return sum
 end
