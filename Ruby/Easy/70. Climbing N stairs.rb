024f99911774227682349f81b70d6a5fec15fa3b
max <max.lillie95@gmail.com>
2023-04-06 19:36:52 -0500

    created a hash table and it worked

--- Ruby/Easy/70. Climbing N stairs.rb
+++ Ruby/Easy/70. Climbing N stairs.rb
@@ -1,9 +1,14 @@
+@memo = {}
+
 def climb_stairs(n)
   if n <= 1
     return n
   elsif n == 2
     return 2
+  elsif @memo[n]
+    return @memo[n]
   else
-    return climb_stairs(n - 1) + climb_stairs(n - 2)
+    @memo[n] = climb_stairs(n - 1) + climb_stairs(n - 2)
+    return @memo[n]
   end
 end
