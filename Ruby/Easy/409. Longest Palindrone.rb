f44a23fb732cbbc417899a8f53f7cd4609f2f884
max <max.lillie95@gmail.com>
2023-06-22 19:22:39 -0500

    fixed 409

--- Ruby/Easy/409. Longest Palindrone.rb
+++ Ruby/Easy/409. Longest Palindrone.rb
@@ -4,13 +4,15 @@ def longest_palindrome(s)
   char_count = Hash.new(0)
   s.each_char { |char| char_count[char] += 1 }
   length = 0
+  has_odd_count = false
   char_count.each do |char, count|
     if count.even?
       length += count
     else
       length += count - 1
+      has_odd_count = true
     end
   end
-  length += 1 if char_count.values.any? { |count| count == 1 }
+  length += 1 if has_odd_count
   return length
 end
