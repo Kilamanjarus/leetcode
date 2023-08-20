facbd400d4742ceec52c42bf37306ebbd5778ce0
max <max.lillie95@gmail.com>
2023-08-19 22:44:11 -0500

    fimished 112, path sum

---
+++ Ruby/Easy/112. Path Sum.rb
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} target_sum
+# @return {Boolean}
+
+def has_path_sum(root, target_sum)
+  return false if root.nil?
+
+  if root.left.nil? && root.right.nil?
+    return root.val == target_sum
+  end
+
+  left_path = has_path_sum(root.left, target_sum - root.val)
+  right_path = has_path_sum(root.right, target_sum - root.val)
+
+  left_path || right_path
+end
