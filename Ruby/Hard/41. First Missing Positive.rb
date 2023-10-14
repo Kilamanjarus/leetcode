7fee2289672c0f2dc4d3baf8a97920a178d8e9e9
max <max.lillie95@gmail.com>
2023-10-13 22:56:37 -0500

    finished 41. First Missing Positive

---
+++ Ruby/Hard/41. First Missing Positive.rb
@@ -0,0 +1,19 @@
+def jump(nums)
+  n = nums.length
+  if n == 1
+    return 0
+  end
+
+  jumps = 0
+  max_reach = 0
+  current_end = 0
+
+  for i in 0..(n - 2)
+    max_reach = [max_reach, i + nums[i]].max
+    if i == current_end
+      jumps += 1
+      current_end = max_reach
+    end
+  end
+  jumps
+end
